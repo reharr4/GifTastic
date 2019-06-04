6bd0a6d7edb108355d3ccce7f0639c80a1919ab2
Harrison, Rachel <reharr4@gmail.com>
2019-06-04 09:48:55 -0500

    gif subject selected, tags,btns, etc updated to reflect

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,17 @@
 
-var animals = ["cat", "dog", "horse", "otter"];
+var cartoons = ["Powerpuff Girls", "Spongebob Squarepants", "Animaniacs", "Super Jail"];
 
 $(document).ready(function () {
 
-   // add on click listener to elements with animal-btn class
-   $(document).on("click", ".animal-btn", displayAnimalInfo);
-    function displayAnimalInfo() {
+   // add on click listener to elements with cartoon-btn class
+   $(document).on("click", ".cartoon-btn", displayCartoonInfo);
+    function displayCartoonInfo() {
 
-        var animal = $(this).attr("data-animal");
-        // giphy api
+        // button clicked 
+        var cartoon = $(this).attr("data-cartoon");
+        // giphy API
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            animal + "&api_key=vVUVgEidbLRXqZbFQj3rwYnqtSXxw0MN&limit=10";
+            cartoon + "&api_key=vVUVgEidbLRXqZbFQj3rwYnqtSXxw0MN&limit=10";
 
         // ajax call for button clicked
         $.ajax({
@@ -18,73 +19,75 @@ $(document).ready(function () {
             method: "GET"
         })
 
+            // data back from API
             .then(function (response) {
+
+                // store results in array
                 var results = response.data;
 
+                // loop through resulting items
                 for (var i = 0; i < results.length; i++) {
-                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
+                    // act if resulting gif has appropriate rating
+                    if (results[i].rating !== "r") {
+
+                        // create div for gif
                         var gifDiv = $("<div>");
+
+                        // store result rating
                         var rating = results[i].rating;
+
+                        // paragraph to hold the rating
                         var p = $("<p>").text("Rating: " + rating);
-                        // create a div to hold the GIF
-                        // var animalDIV = $("<div class='animal'>");
-
-                        // store the rating
-                        // var rating = response.Rated;
-
-                        // display the rating
-                        // animalDIV.append(pOne);
-
-                        // get URL for image
-                        // var imgURL = response.Gif;
 
                         // element to hold image
-                        var animalImage = $("<img>")
+                        var cartoonImage = $("<img>")
 
-                        animalImage.attr("src", results[i].images.fixed_height.url);
+                        // give image tag src attribute of property pulled from result
+                        cartoonImage.attr("src", results[i].images.fixed_height.url);
 
-                        // append gif
+                        // append gif and rating to div
                         gifDiv.append(p);
-                        gifDiv.append(animalImage);
+                        gifDiv.append(cartoonImage);
 
-                        // add animal above previous animals
-                        $("#animal-view").prepend(gifDiv);
+                        // add cartoon above previous cartoons
+                        $("#cartoon-view").prepend(gifDiv);
                     }
                 }
-
+                $("")
 
             });
     }
 
-    // function to display animal data
+    // function to display cartoon data
     function renderButtons() {
         // empty form prior to adding new gifs
         $("#buttons-view").empty();
 
         // loop through array of GIF
-        for (var i = 0; i < animals.length; i++) {
+        for (var i = 0; i < cartoons.length; i++) {
             // generate buttons for each GIF in the array
             var a = $("<button>");
             // add class of GIF to button
-            a.addClass("animal-btn");
+            a.addClass("cartoon-btn");
             // add data-attribute
-            a.attr("data-animal", animals[i]);
+            a.attr("data-cartoon", cartoons[i]);
             // button text
-            a.text(animals[i]);
+            a.text(cartoons[i]);
             // add button to buttons-view div
             $("#buttons-view").append(a);
         }
     }
 
-    // animal button on click function
-    $("#add-animal").on("click", function (event) {
+    // cartoon button on click function
+    $("#add-cartoon").on("click", function (event) {
         // press enter or submit
         event.preventDefault();
         // grab input from textbox
-        var animal = $("#animal-input").val().trim();
+        var tv = $("#cartoon-input").val().trim();
 
-        // add animal from textbox to array
-        animals.push(animal);
+        // add cartoon from textbox to array
+        cartoons.push(tv);
 
         // call renderButtons to process array
         renderButtons();
